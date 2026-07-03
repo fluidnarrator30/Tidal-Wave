ee017550b731882924d672de8c5d46072a93fd7f
fluidnarrator30 <ramosfamily2011b@gmail.com>
2026-07-02 20:13:16 -0400

    Update loadstring to use Loader.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fluidnarrator30/Tidal-Wave/refs/heads/main/TidalWave.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/fluidnarrator30/Tidal-Wave/refs/heads/main/Loader.lua"))()
